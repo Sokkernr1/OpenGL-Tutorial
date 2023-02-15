cec02936d8409240f8e67c104a5f3a74e13620ac
Paul Sohns <paul.sohns@web.de>
2023-02-15 19:33:01 +0100

    Complete second tutorial step

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // Include standard headers
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 
 // Include GLEW. Always include it before gl.h and glfw3.h, since it's a bit magic.
 #include <GL/glew.h>
@@ -12,13 +12,15 @@
 #include <glm/glm.hpp>
 using namespace glm;
 
+#include "src/shader/loadShader.h"
+
 int main()
 {
     // Initialise GLFW
     glewExperimental = true; //Needed for core profile
     if ( !glfwInit() )
     {
-        fprintf( stderr, "Failed to initialize GLFW!\n" );
+        fprintf(stderr, "Failed to initialize GLFW!\n");
         return -1;
     }
 
@@ -29,10 +31,10 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window;
-    window = glfwCreateWindow( 1024, 640, "OpenGL", nullptr, nullptr );
+    window = glfwCreateWindow(1024, 640, "OpenGL", nullptr, nullptr);
     if ( window == nullptr )
     {
-        fprintf( stderr, "Failed to open GLFW window...\n" );
+        fprintf(stderr, "Failed to open GLFW window...\n");
         glfwTerminate();
         return -1;
     }
@@ -47,10 +49,50 @@ int main()
 
     glfwSetInputMode( window, GLFW_STICKY_KEYS, GL_TRUE);
 
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f
+    };
+
+    // Identify the vertex buffer
+    GLuint vertexBuffer;
+    // Generate a buffer with our identifier
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    // Give vertices to OpenGL
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders( "resources/shader/simpleVertexShader.vert", "resources/shader/simpleFragmentShader.frag" );
+
     do
     {
-        glClear( GL_COLOR_BUFFER_BIT ); //Clear the screen
-        glfwSwapBuffers( window );
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear the screen
+
+        // Use our shader
+        glUseProgram(programID);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(
+            0,             // No particular reason for 0, but must match the layout in the shader
+            3,             // Size
+            GL_FLOAT,      // Type
+            GL_FALSE,      // Normalized?
+            0,             // Stride
+            (void*)nullptr // Array buffer offset
+        );
+
+        // Drawing the triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Start at vertex 0 -> 3 Vertices -> 1 Triangle
+        glDisableVertexAttribArray(0);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
