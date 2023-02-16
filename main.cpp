5a2cf89f4b212fd4f3bb07438660185b10388f71
Paul Sohns <paul.sohns@web.de>
2023-02-16 15:01:52 +0100

    Complete step 4 and print FPS

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 // Include standard headers
-#include <cstdio>
 #include <cstdlib>
+#include <stdio.h>
+#include <string>
+#include <iostream>
 
 // Include GLEW. Always include it before gl.h and glfw3.h, since it's a bit magic.
 #include <GL/glew.h>
@@ -54,11 +56,59 @@ int main()
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
 
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+
+    // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+    // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
     static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f
+            -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+            -1.0f,-1.0f, 1.0f,
+            -1.0f, 1.0f, 1.0f, // triangle 1 : end
+            1.0f, 1.0f,-1.0f, // triangle 2 : begin
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f,-1.0f, // triangle 2 : end
+            1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f, 1.0f,
+            -1.0f,-1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f,-1.0f,
+            -1.0f, 1.0f,-1.0f,
+            1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f,-1.0f,
+            -1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f
     };
+
+    // One color for each vertex. They were generated randomly.
+    static GLfloat g_color_buffer_data[12*3*3];
+    for (int v = 0; v < 12*3 ; v++){
+        g_color_buffer_data[3*v+0] = 0;
+        g_color_buffer_data[3*v+1] = 0;
+        g_color_buffer_data[3*v+2] = 0;
+    }
 
     // Identify the vertex buffer
     GLuint vertexBuffer;
@@ -67,6 +117,10 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     // Give vertices to OpenGL
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint colorBuffer;
+    glGenBuffers(1, &colorBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
 
     // Create and compile our GLSL program from the shaders
     GLuint programID = LoadShaders( "resources/shader/simpleVertexShader.vert", "resources/shader/simpleFragmentShader.frag" );
@@ -87,9 +141,9 @@ int main()
 
     // Camera matrix
     glm::mat4 View = glm::lookAt(
-        glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+        glm::vec3(4,3,-3), // Camera is at (4,3,3), in World Space
         glm::vec3(0,0,0), // and looks at the origin
-        glm::vec3(-1,0,0)  // Head is up (set to 0,-1,0 to look upside-down)
+        glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
     );
 
     // Model matrix : an identity matrix (model will be at the origin)
@@ -97,8 +151,38 @@ int main()
     // Our ModelViewProjection : multiplication of our 3 matrices
     glm::mat4 mvp = Projection * View * Model; // Matrix multiplication is the other way around
 
+    float colorChangeValue = 0.01f;
+    double lastTime = glfwGetTime();
+    int totalFrames = 0;
     do
     {
+        double currentTime = glfwGetTime();
+        totalFrames++;
+        if ( currentTime - lastTime >= 1.0 )
+        {
+            std::cout << "FPS: " + std::to_string(1000.0 / double(totalFrames)) + "\n";
+            totalFrames = 0;
+            lastTime += 1.0;
+        }
+
+        if ( g_color_buffer_data[3*0+2] >= 1 && colorChangeValue > 0.0f )
+        {
+            colorChangeValue = -0.01f;
+        }
+        else if ( g_color_buffer_data[3*0+2] <= 0 && colorChangeValue < 0.0f )
+        {
+            colorChangeValue = 0.01f;
+        }
+
+        for (int v = 0; v < 12*3 ; v++)
+        {
+            g_color_buffer_data[3 * v + 0] = g_color_buffer_data[3 * v + 0] + colorChangeValue;
+            g_color_buffer_data[3 * v + 1] = g_color_buffer_data[3 * v + 1] + colorChangeValue;
+            g_color_buffer_data[3 * v + 2] = g_color_buffer_data[3 * v + 2] + colorChangeValue;
+        }
+
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear the screen
 
         // Use our shader
@@ -119,8 +203,19 @@ int main()
             (void*)nullptr // Array buffer offset
         );
 
-        // Drawing the triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3); // Start at vertex 0 -> 3 Vertices -> 1 Triangle
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+        glVertexAttribPointer(
+                1,             // No particular reason for 0, but must match the layout in the shader
+                3,             // Size
+                GL_FLOAT,      // Type
+                GL_FALSE,      // Normalized?
+                0,             // Stride
+                (void*)nullptr // Array buffer offset
+        );
+
+        // Drawing the object
+        glDrawArrays(GL_TRIANGLES, 0, sizeof(g_vertex_buffer_data) / 3); // Start at vertex 0 -> 3 Vertices -> 1 Triangle
         glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
